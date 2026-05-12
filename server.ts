27cf26ec906ce08dfff4026c87f84ce4775efe7f
alexradull9999-sudo <alexradull9999@gmail.com>
2026-05-12 07:21:46 +0300

    feat: Use environment variable for port
    Configures the server to use the `PORT` environment variable if set, defaulting to 3000. This allows for flexible deployment configurations.
    Also removes unused nodemailer import.

--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import axios from "axios";
-import nodemailer from "nodemailer";
 
 dotenv.config();
 
@@ -13,7 +12,7 @@ const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
